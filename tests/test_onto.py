acbf6da4a9bdfec789e94552d0c71a5c1fefa897
brandon <bjd2385.linux@gmail.com>
2018-06-26 04:51:31 -0400

    Added onto test

--- tests/test_onto.py
+++ tests/test_onto.py
@@ -18,7 +18,7 @@ class TestOnto(unittest.TestCase):
 
     ∀pϵParts, ∃qϵQuestions s. th. F(q) -> p.
 
-    This test is F.
+    This test is the mapping F. Note that it doesn't have to be a function or 1-1.
     """
 
     _processes_mapped_to = set()
@@ -29,10 +29,15 @@ class TestOnto(unittest.TestCase):
         self.parts = self.data['V6Parts']
         self.part_numbers_set = set(self.parts.keys())
         self.questions = self.data['questions']
+        self.processes = set(self.data['processes'])
 
         self.len_parts = len(self.parts)
+        self.len_processes = len(self.processes)
 
-    def test_onto(self) -> None:
+    def test_onto_parts(self) -> None:
+        """
+        Ensure the parts are mapped onto.
+        """
         # Get the set of parts mapped to
         for level in self.questions.values():
             for question in level.values():
@@ -43,20 +48,31 @@ class TestOnto(unittest.TestCase):
         # datastructure, where collateral parts are trimmed. I iterate
         # `self.len_parts` times here because it's guaranteed to catch all
         # collateral parts.
-        for i in range(self.len_parts):
+        for _ in range(self.len_parts):
             collateralParts = set()
             for part in self._parts_mapped_to:
                 collateralParts |= set(self.parts[part][2]['collateralParts'])
             self._parts_mapped_to |= collateralParts
-
-        # Get the set of processes mapped to
-        for level in self.questions.values():
-            for question in level.values():
-                self._processes_mapped_to |= set(question['processes'])
-        self._processes_mapped_to.discard('')
 
         warn(f'These parts aren\'t in the image: '
              f'{self.part_numbers_set - self._parts_mapped_to}')
 
         self.assertEqual(self.len_parts, len(self._parts_mapped_to),
             msg='Questions must cover every part in the provided parts list')
+
+    @unittest.skip
+    def test_onto_processes(self) -> None:
+        """
+        Ensure the processes are mapped onto (less important).
+        """
+        # Get the set of processes mapped to
+        for level in self.questions.values():
+            for question in level.values():
+                self._processes_mapped_to |= set(question['processes'])
+        self._processes_mapped_to.discard('')
+
+        warn(f'These processes are not utilized: '
+             f'{self.processes - self._processes_mapped_to}')
+
+        self.assertEqual(self.len_processes, len(self._processes_mapped_to),
+            msg='Processes are not all mapped to')
